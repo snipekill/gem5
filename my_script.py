d97242550db512073137aafd3ad3e60dbe3cf715
snipekill <apoorv.garg.cse15@gmail.com>
2022-02-20 19:13:02 -0800

    mem: fix script

--- my_script.py
+++ my_script.py
@@ -5,17 +5,28 @@ benchmarks = ['619.lbm_s', '538.imagick_r', '644.nab_s', '625.x264_s', '605.mcf_
 REPLACEMENT_POLICIES = ['NMRURP', 'RANDOMRP', 'LIPRP']
 
 for REPLACEMENT_POLICY in REPLACEMENT_POLICIES:
-    bashCommand = "rm -rf m5out/"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
     bashCommand = "make simall REPLACEMENT_POLICY=" + REPLACEMENT_POLICY
-    bashCommand += "CACHE_ASSOC=8"
+    bashCommand += " CACHE_ASSOC=8"
+    print(bashCommand)
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     for benchmark in benchmarks:
         bashCommand = "cp -a " + "benchspec/CPU/"
         bashCommand+= benchmark + "/run/run_base_refspeed_mytest-m64.0000/m5out/"
-        bashCommand+= "../hw3-results/" + REPLACEMENT_POLICY + "/" + benchmark + "/"
+        bashCommand+= " ../hw3-results/" + REPLACEMENT_POLICY + "/" + benchmark + "/"
+        print(bashCommand)
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
 
+bashCommand = "make simall REPLACEMENT_POLICY=LIPRP"
+bashCommand += " CACHE_ASSOC=8"
+print(bashCommand)
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
+for benchmark in benchmarks:
+    bashCommand = "cp -a " + "benchspec/CPU/"
+    bashCommand+= benchmark + "/run/run_base_refspeed_mytest-m64.0000/m5out/"
+    bashCommand+= " ../hw3-results/LIPRP-4/" + benchmark + "/"
+    print(bashCommand)
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
